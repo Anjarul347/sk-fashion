1512679c802d275ab1340a8e725368974942af3f
Sk ANjarul Hossain <skanjarulhossain347@gmail.com>
2024-01-23 00:17:29 +0530

    Second

---
+++ src/customer/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainCarousel from '../components/homecarousel/MainCarousel';
+
+const Homepage = () => {
+  return (
+    <div>
+      <MainCarousel/>
+      <div>
+        Other section
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
